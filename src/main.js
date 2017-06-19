e45d6e4f43cbb05655cf7a3bfd1046009944cd09
Ahmet Ozisik <ozisikahmet@gmail.com>
2017-06-19 23:31:23 +0300

    implement checking nvidia card stats

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+var shell = require('shelljs');
 var admin = require("firebase-admin");
 var serviceAccount = require("../serviceAccount.json");
 
@@ -5,3 +6,17 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://dwarven-miners.firebaseio.com"
 });
+
+var db = admin.database();
+var ref = db.ref('rig1/stats');
+
+ref.set({
+	test: "hello world"
+});
+
+setInterval(function() {
+
+	var cmd = shell.exec("nvidia-smi --query-gpu=index,name,temperature.gpu,clocks.current.video --format=csv");
+	console.log(cmd);
+
+}, 5000);

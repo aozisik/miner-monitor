ad154793b915424c5f1497e4f0e19aaedf749c7f
Ahmet Ozisik <ozisikahmet@gmail.com>
2017-06-23 11:01:52 +0300

    lint

--- src/shell.js
+++ src/shell.js
@@ -1,31 +1,31 @@
-const _ = require('lodash');
-const shell = require('shelljs');
-const Promise = require('bluebird');
+const _ = require("lodash");
+const shell = require("shelljs");
+const Promise = require("bluebird");
 
 function Shell(command) {
 
-	this.arg = (argument, value) => {
-		if(_.isArray(value)) {
-			value = value.join(",");
-		}
-		command += " --" + argument + "=" + value;
-		return this;
-	};
+    this.arg = (argument, value) => {
+        if (_.isArray(value)) {
+            value = value.join(",");
+        }
+        command += " --" + argument + "=" + value;
+        return this;
+    };
 
-	this.run = () => {
-		return new Promise((resolve, reject) => {
-			shell.exec(command, function(code, stdout, stderr) {
-				if(stderr) {
-					reject(new Error(stderr));
-					return;
-				}
-				resolve(stdout);
-				return;
-			});
-		});
-	};
+    this.run = () => {
+        return new Promise((resolve, reject) => {
+            shell.exec(command, function(code, stdout, stderr) {
+                if (stderr) {
+                    reject(new Error(stderr));
+                    return;
+                }
+                resolve(stdout);
+                return;
+            });
+        });
+    };
 }
 
 module.exports = function(cmd) {
-	return new Shell(cmd);
+    return new Shell(cmd);
 };

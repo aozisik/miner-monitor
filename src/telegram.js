dffec85f4dca1a5ae39b2d39eae68f234d06f796
Ahmet Ozisik <ozisikahmet@gmail.com>
2017-07-12 18:56:57 +0300

    add new bot query

--- src/telegram.js
+++ src/telegram.js
@@ -1,7 +1,11 @@
 const _ = require('lodash');
+const request = require('request-json');
+const client = request.createClient(process.env.API_URL);
+//
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.TELEGRAM_ACCESS_TOKEN, {polling: true});
 const chatId = process.env.TELEGRAM_CHAT_ID;
+//
 let information = null;
 
 bot.onText(/\/rig/, (msg, match) => {
@@ -15,6 +19,24 @@ bot.onText(/\/rig/, (msg, match) => {
 		}).join("\n");
 	}
 	bot.sendMessage(chatId, reply);
+});
+
+bot.onText(/\/eth/, (msg, match) => {
+	if(msg.chat.id != chatId) {
+		return; // Drop foreign requests
+	}
+	client.get('api/stats', function(err, res, body) {
+		if(err) {
+			bot.sendMessage(chatId, 'Bilgileri alamadım. Kusura bakma...');
+			return;
+		}
+		bot.sendMessage(chatId, [
+			'1 Eth = $' + body.ethPriceInUsd,
+			'1 Eth = ' + body.ethPriceInTry + '₺',
+			'Anlık HashRate = ' + body.currentHashRate,
+			'Ödenmemiş ETH = ' + body.unpaidEth.toFixed(6)
+		].join("\n"));
+	});
 });
 
 module.exports = {
